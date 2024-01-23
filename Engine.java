e9756895eab773accad3db327ea9496e39b12cc7
maximocaratini <mac8373@psu.edu>
2024-01-22 21:03:42 -0500

    Commit

---
+++ Engine.java
@@ -0,0 +1,21 @@
+/**
+ * Project: lab1Caratini
+ * Purpose Details: Representing engine liter size of motorcycle.
+ * Course: IST 242
+ * Author: Maximo Caratini
+ * Date Developed: 1/22/24
+ * Last Date Changed: 1/22/24
+ * Rev: 1.0
+ */
+
+public class Engine {
+    private double engineLiterSize;
+
+    public Engine(double engineLiterSize) {
+        this.engineLiterSize = engineLiterSize;
+    }
+
+    public double getEngineLiterSize() {
+        return engineLiterSize;
+    }
+}
