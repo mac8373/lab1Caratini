c987e8b48a847711c77a0e127a7267f591d6fe4c
maximocaratini <mac8373@psu.edu>
2024-01-22 20:44:10 -0500

    Commit

---
+++ Model.java
@@ -0,0 +1,21 @@
+/**
+ * Project: lab1Caratini
+ * Purpose Details: Representing model of motorcycle.
+ * Course: IST 242
+ * Author: Maximo Caratini
+ * Date Developed: 1/22/24
+ * Last Date Changed: 1/22/24
+ * Rev: 1.0
+ */
+
+public class Model {
+    private String model;
+
+    public Model(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+}
