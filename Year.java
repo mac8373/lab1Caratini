6de2a0275c5b1571c2a4aae7303b975928ad0f47
maximocaratini <mac8373@psu.edu>
2024-01-22 20:54:26 -0500

    Commit

---
+++ Year.java
@@ -0,0 +1,21 @@
+/**
+ * Project: lab1Caratini
+ * Purpose Details: Representing year of motorcycle.
+ * Course: IST 242
+ * Author: Maximo Caratini
+ * Date Developed: 1/22/24
+ * Last Date Changed: 1/22/24
+ * Rev: 1.0
+ */
+
+public class Year {
+    private String year;
+
+    public Year(String year) {
+        this.year = year;
+    }
+
+    public String getYear() {
+        return year;
+    }
+}
