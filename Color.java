34ed62c270f5a0a8b6645a246011428aaa7dbcc7
maximocaratini <mac8373@psu.edu>
2024-01-22 20:59:29 -0500

    Commit

---
+++ Color.java
@@ -0,0 +1,21 @@
+/**
+ * Project: lab1Caratini
+ * Purpose Details: Representing color of motorcycle.
+ * Course: IST 242
+ * Author: Maximo Caratini
+ * Date Developed: 1/22/24
+ * Last Date Changed: 1/22/24
+ * Rev: 1.0
+ */
+
+public class Color {
+    private String color;
+
+    public Color(String color) {
+        this.color = color;
+    }
+
+    public String getcolor() {
+        return color;
+    }
+}
