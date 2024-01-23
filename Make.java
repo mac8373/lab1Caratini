6506d638c18cc7c48d5c97a88b7484fc2e5f3103
maximocaratini <mac8373@psu.edu>
2024-01-22 20:38:06 -0500

    Commit

---
+++ Make.java
@@ -0,0 +1,21 @@
+/**
+ * Project: lab1Caratini
+ * Purpose Details: Representing brand of motorcycle
+ * Course: IST 242
+ * Author: Maximo Caratini
+ * Date Developed: 1/22/24
+ * Last Date Changed: 1/22/24
+ * Rev: 1.0
+ */
+
+public class Make {
+    private String make;
+
+    public Make(String make) {
+        this.make = make;
+    }
+
+    public String getMake() {
+        return make;
+    }
+}
